f70ffa45b2a734789b50c1a9f61fcfea488e22c6
Reza Kargar <r.kargar.2014@gmail.com>
2020-08-30 17:10:38 +0430

    Configure Serilog to be the default logger

--- src/KodoomOstad.WebApi/Program.cs
+++ src/KodoomOstad.WebApi/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace KodoomOstad.WebApi
 {
@@ -13,11 +14,35 @@ namespace KodoomOstad.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            // Set an environment variable for app's start directory
+            // to use it in appsettings.json for saving the logs there
+            Environment.SetEnvironmentVariable("BASEDIR", AppDomain.CurrentDomain.BaseDirectory);
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = 
+                new LoggerConfiguration().ReadFrom.Configuration(configuration).CreateLogger();
+
+            try
+            {
+                Log.Information("Starting...");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Start failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
