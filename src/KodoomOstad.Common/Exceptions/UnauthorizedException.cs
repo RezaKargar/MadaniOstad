4365ae712e895652958432cd6ed38ad71c7ca045
Reza Kargar <r.kargar.2014@gmail.com>
2020-08-31 21:56:40 +0430

    Add new exceptions for Forbidden and Unauthorized

---
+++ src/KodoomOstad.Common/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace KodoomOstad.Common.Exceptions
+{
+    public class UnauthorizedException : AppException
+    {
+        public UnauthorizedException(string message = "Unauthorized Exception")
+            : base(message, HttpStatusCode.Unauthorized)
+        {
+        }
+    }
+}
