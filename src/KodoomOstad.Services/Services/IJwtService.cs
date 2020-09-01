053a5ff37306b1356c8eda68a95ad2f8a25e6f57
Reza Kargar <r.kargar.2014@gmail.com>
2020-09-01 15:15:07 +0430

    Add files that left from previous commit for Jwt
    Add Jwt services left form implementing jwt

---
+++ src/KodoomOstad.Services/Services/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using KodoomOstad.Entities.Models;
+using System.Threading.Tasks;
+
+namespace KodoomOstad.Services.Services
+{
+    public interface IJwtService
+    {
+        Task<AccessToken> GenerateAsync(User user);
+    }
+}
