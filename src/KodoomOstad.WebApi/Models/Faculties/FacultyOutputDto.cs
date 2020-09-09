f44954a62a43898e5520f9227813b5fee9baaa42
Reza Kargar <r.kargar.2014@gmail.com>
2020-09-09 21:14:59 +0430

    Fix wrong type of Id property in FacultyOutputDto
    Change type of Id property to be 'int' not 'string'

--- src/KodoomOstad.WebApi/Models/Faculties/FacultyOutputDto.cs
+++ src/KodoomOstad.WebApi/Models/Faculties/FacultyOutputDto.cs
@@ -5,7 +5,7 @@ namespace KodoomOstad.WebApi.Models.Faculties
 {
     public class FacultyOutputDto : IMapFrom<Faculty>
     {
-        public string Id { get; set; }
+        public int Id { get; set; }
 
         public string Name { get; set; }
 
