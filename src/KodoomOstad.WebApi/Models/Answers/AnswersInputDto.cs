ac98b84ceaeb44339aadf8398f322f208b559524
Reza Kargar <r.kargar.2014@gmail.com>
2020-09-11 13:44:57 +0430

    Add AnswersContrller and its model dtos

---
+++ src/KodoomOstad.WebApi/Models/Answers/AnswersInputDto.cs
@@ -0,0 +1,16 @@
+﻿using KodoomOstad.Entities.Models;
+using KodoomOstad.IocConfig.CustomMapping;
+
+namespace KodoomOstad.WebApi.Models.Answers
+{
+    public class AnswersInputDto : IMapTo<Answer>
+    {
+        public int Score { get; set; }
+
+        public int ProfessorId { get; set; }
+
+        public int PollQuestionId { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ src/KodoomOstad.WebApi/Models/Answers/AnswersUpdateInputDto.cs
@@ -0,0 +1,10 @@
+﻿using KodoomOstad.Entities.Models;
+using KodoomOstad.IocConfig.CustomMapping;
+
+namespace KodoomOstad.WebApi.Models.Answers
+{
+    public class AnswersUpdateInputDto : IMapTo<Answer>
+    {
+        public int Score { get; set; }
+    }
+}
